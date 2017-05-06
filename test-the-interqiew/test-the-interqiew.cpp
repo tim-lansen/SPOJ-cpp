24ec80f6f52d522eb9a5b09c551a13557fcf262d
Tim <tim.lansen@gmail.com>
2017-05-06 21:54:47 +0300

    test-the-interqiew

---
+++ test-the-interqiew/test-the-interqiew.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+class A
+{
+public:
+    A(int n = 0): m_n(n)
+    {
+    }
+
+public:
+    virtual int f() const
+    {
+        return m_n;
+    }
+    virtual ~A() {}
+
+protected:
+    int m_n;
+};
+
+class B
+    : public A
+{
+public:
+    B(int n = 0): A(n)
+    {
+    }
+
+public:
+    virtual int f() const
+    {
+        return m_n + 1;
+    }
+};
+
+int main()
+{
+    const A a(1);
+    const B b(3);
+    const A *x[2] = {&a, &b};
+    typedef std::vector<A> V;
+    V y({a, b});
+    V::const_iterator i = y.begin();
+
+    std::cout << x[0]->f() << x[1]->f()
+        << i->f() << (i + 1)->f() << std::endl;
+
+    return 0;
+}
